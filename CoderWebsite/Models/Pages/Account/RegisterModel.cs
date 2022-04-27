d264f6230369c2e55b07503684487572c675f285
Lisowolf <minebuilder445@gmail.com>
2022-04-27 21:47:30 +0600

    Добавил регистрацию

---
+++ CoderWebsite/Models/Pages/Account/RegisterModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoderWebsite.Models.Pages.Account
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string? Firstname { get; set; }
+
+        [Required]
+        public string? Lastname { get; set; }
+
+        public string? Middlename { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        [Compare("Password")]
+        public string? PasswordConfirmation { get; set; }
+    }
+}

24d2674d634f749b405bfe1d6ac2b540506160a1
Lisowolf <minebuilder445@gmail.com>
2022-04-27 21:53:14 +0600

    Перевел сообщения при валидации

--- CoderWebsite/Models/Pages/Account/LoginModel.cs
+++ CoderWebsite/Models/Pages/Account/LoginModel.cs
@@ -4,13 +4,13 @@ namespace CoderWebsite.Models.Pages.Account
 {
     public class LoginModel
     {
-        [Required]
+        [Required(ErrorMessage = "Поле адреса электронной почты должно быть заполнено.")]
         [EmailAddress]
         public string? Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [MinLength(8)]
+        [Required(ErrorMessage = "Поле пароля должно быть заполнено.")]
+        [DataType(DataType.Password, ErrorMessage = "Введенный текст не является паролем.")]
+        [MinLength(8, ErrorMessage = "Длина пароля должна быть не менее 8 символов..")]
         public string? Password { get; set; }
 
         public bool RememberMe { get; set; }
--- CoderWebsite/Models/Pages/Account/RegisterModel.cs
+++ CoderWebsite/Models/Pages/Account/RegisterModel.cs
@@ -4,27 +4,27 @@ namespace CoderWebsite.Models.Pages.Account
 {
     public class RegisterModel
     {
-        [Required]
+        [Required(ErrorMessage = "Поле имени должно быть заполнено.")]
         public string? Firstname { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле фамилии должно быть заполнено.")]
         public string? Lastname { get; set; }
 
         public string? Middlename { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Поле адреса электронной почты должно быть заполнено.")]
         [EmailAddress]
         public string? Email { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [MinLength(8)]
+        [Required(ErrorMessage = "Поле пароля должно быть заполнено.")]
+        [DataType(DataType.Password, ErrorMessage = "Введенный текст не является паролем.")]
+        [MinLength(8, ErrorMessage = "Длина пароля должна быть не менее 8 символов.")]
         public string? Password { get; set; }
 
-        [Required]
-        [DataType(DataType.Password)]
-        [MinLength(8)]
-        [Compare("Password")]
+        [Required(ErrorMessage = "Поле повторного ввода пароля должно быть заполнено.")]
+        [DataType(DataType.Password, ErrorMessage = "Введенный текст не является паролем.")]
+        [MinLength(8, ErrorMessage = "Длина подтверждаемого пароля должна быть не менее 8 символов.")]
+        [Compare("Password", ErrorMessage = "Пароли должны совпадать.")]
         public string? PasswordConfirmation { get; set; }
     }
 }

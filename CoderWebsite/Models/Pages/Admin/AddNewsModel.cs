1a95be9de4723f69e5a88de00f54daac4ba2b59e
Lisowolf <minebuilder445@gmail.com>
2022-05-02 00:56:37 +0600

    Добавил страницу добавления новости

---
+++ CoderWebsite/Models/Pages/Admin/AddNewsModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoderWebsite.Models.Pages.Admin
+{
+    public class AddNewsModel
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+    }
+}

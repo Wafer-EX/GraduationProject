192cc67979148b6e722e13e9dda6086251ea89e5
Lisowolf <minebuilder445@gmail.com>
2022-05-08 16:47:34 +0600

    Добавил страницу оформления заказа

---
+++ CoderWebsite/Components/Account/OrderType.cs
@@ -0,0 +1,10 @@
+﻿namespace CoderWebsite.Components.Account
+{
+    public enum OrderAppType
+    {
+        Website_PWA,
+        Website_SPA,
+        Microservice_gRPC,
+        Microservice_ZeroC_Ice
+    }
+}
---
+++ CoderWebsite/Models/Pages/Account/OrderingModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoderWebsite.Models.Pages.Account
+{
+    public class OrderingModel
+    {
+        [Required(ErrorMessage = "Поле ФИО должно быть заполнено.")]
+        public string FIO { get; set; }
+
+        [Required(ErrorMessage = "Поле номер телефона должно быть заполнено.")]
+        [Phone(ErrorMessage = "Данные в поле номер телефона не являются номером телефона.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Поле электронная почта должно быть заполнено.")]
+        [EmailAddress(ErrorMessage = "Данные в поле электронная почта не являются электронной почтой.")]
+        public string Email { get; set; }
+    }
+}

53fe2f0bb788e6d9890e113fca77af2eeeec952e
Lisowolf <minebuilder445@gmail.com>
2022-05-30 16:51:51 +0600

    Добавил страницу обратной связи

---
+++ CoderWebsite/Models/Pages/FeedbackModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoderWebsite.Models.Pages
+{
+    public class FeedbackModel
+    {
+        [Required(ErrorMessage = "Электронная почта является обязательной.")]
+        [EmailAddress(ErrorMessage = "Введенное значение не является адресом электронной почты")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Сообщение должно содержать текст.")]
+        public string Message { get; set; }
+    }
+}

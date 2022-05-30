4cf4255277751f86400ef4095ee9705055273467
Lisowolf <minebuilder445@gmail.com>
2022-05-30 18:28:59 +0600

    Всякие штуки про голосование (в процессе)

---
+++ CoderWebsite/Components/Voting/VoringVariants.cs
@@ -0,0 +1,11 @@
+﻿namespace CoderWebsite.Components.Voting
+{
+    public enum VoringVariants
+    {
+        Variant1,
+        Variant2,
+        Variant3,
+        Variant4,
+        Variant5
+    }
+}

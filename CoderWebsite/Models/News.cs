4902f43d02acd905a5f6f984186eacb21e2573f2
Lisowolf <minebuilder445@gmail.com>
2022-06-04 00:19:25 +0600

    Обновил зависимости

--- CoderWebsite/Models/News.cs
+++ CoderWebsite/Models/News.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
 
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
-        public string Text { get; set; }
+        public string? Text { get; set; }
 
-        public string Date { get; set; }
+        public string? Date { get; set; }
     }
 }
